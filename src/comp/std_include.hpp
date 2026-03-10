252b44b877c207a9a70914d92c31f116ad92a469
#xoxor4d <r4d0xzz@live.de>
2026-03-10 21:25:13 +0100

    bump version to 0.9.7

--- src/comp/std_include.hpp
+++ src/comp/std_include.hpp
@@ -2,7 +2,7 @@
 
 constexpr auto COMP_MOD_VERSION_MAJOR = 0;
 constexpr auto COMP_MOD_VERSION_MINOR = 9;
-constexpr auto COMP_MOD_VERSION_PATCH = 6;
+constexpr auto COMP_MOD_VERSION_PATCH = 7;
 
 #define _CRT_SECURE_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN
